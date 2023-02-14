a31daa8fa2cf679645e272d9c35363776ac318d6
Hugo Jeannin <hjeannin@virtuosgames.com>
2023-02-14 14:36:30 +0100

    more clear code

--- source/main.cpp
+++ source/main.cpp
@@ -1,27 +1,25 @@
-/*
- * Copyright 2011-2019 Branimir Karadzic. All rights reserved.
- * License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
- */
-#include <stdio.h>
-#include <bx/bx.h>
+// BGFX
 #include <bgfx/bgfx.h>
 #include <bgfx/platform.h>
-#include <GLFW/glfw3.h>
-#define GLFW_EXPOSE_NATIVE_WIN32
-#include <GLFW/glfw3native.h>
-#include <bx/math.h>
 #include <bgfx/embedded_shader.h>
-#include <dg_dmapack_vs.sc.glsl.bin.h>
-#include <dg_dmapack_vs.sc.essl.bin.h>
-#include <dg_dmapack_vs.sc.spv.bin.h>
-#include <dg_dmapack_fs.sc.glsl.bin.h>
-#include <dg_dmapack_fs.sc.essl.bin.h>
-#include <dg_dmapack_fs.sc.spv.bin.h>
+
+// GLFW
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+
+// Shader
+#include <shader_vs.sc.glsl.bin.h>
+#include <shader_vs.sc.essl.bin.h>
+#include <shader_vs.sc.spv.bin.h>
+#include <shader_fs.sc.glsl.bin.h>
+#include <shader_fs.sc.essl.bin.h>
+#include <shader_fs.sc.spv.bin.h>
 #ifdef _WIN32
-#include <dg_dmapack_vs.sc.dx9.bin.h>
-#include <dg_dmapack_vs.sc.dx11.bin.h>
-#include <dg_dmapack_fs.sc.dx9.bin.h>
-#include <dg_dmapack_fs.sc.dx11.bin.h>
+#include <shader_vs.sc.dx9.bin.h>
+#include <shader_vs.sc.dx11.bin.h>
+#include <shader_fs.sc.dx9.bin.h>
+#include <shader_fs.sc.dx11.bin.h>
 #endif // _WIN32
 
 static void glfw_keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -30,51 +28,38 @@ static void glfw_keyCallback(GLFWwindow* window, int key, int scancode, int acti
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-const bgfx::EmbeddedShader dg_dmapack_vs_embed = BGFX_EMBEDDED_SHADER(dg_dmapack_vs);
-const bgfx::EmbeddedShader dg_dmapack_fs_embed = BGFX_EMBEDDED_SHADER(dg_dmapack_fs);
+const bgfx::EmbeddedShader shader_vs_embed = BGFX_EMBEDDED_SHADER(shader_vs);
+const bgfx::EmbeddedShader shader_fs_embed = BGFX_EMBEDDED_SHADER(shader_fs);
 
-struct PosVertex
+static float s_vertices[] =
 {
-	float m_x;
-	float m_y;
-	float m_z;
-
-	static void init()
-	{
-		ms_layout
-			.begin()
-			.add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
-			.end();
-	};
-
-	static bgfx::VertexLayout ms_layout;
+	-0.5f, -0.5f,  0.5f,
+	 0.5f,  0.5f,  0.5f,
+	-0.5f,  0.5f,  0.5f,
 };
 
-bgfx::VertexLayout PosVertex::ms_layout;
-
-static PosVertex s_vertices[] =
+void mainLoop(GLFWwindow* window)
 {
-	{-0.5f, -0.5f,  0.5f},
-	{ 0.5f,  0.5f,  0.5f},
-	{-0.5f,  0.5f,  0.5f},
-};
+	// Vertex layout
+	bgfx::VertexLayout layout;
+	layout
+		.begin()
+		.add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+		.end();
 
-void render(GLFWwindow* window)
-{
 	// Buffer
-	PosVertex::init();
-	bgfx::VertexBufferHandle m_vbh;
-	m_vbh = bgfx::createVertexBuffer(bgfx::makeRef(s_vertices, sizeof(s_vertices)), PosVertex::ms_layout);
+	bgfx::VertexBufferHandle vbh;
+	vbh = bgfx::createVertexBuffer(bgfx::makeRef(s_vertices, sizeof(s_vertices)), layout);
 
 	// Shader
-	bgfx::ProgramHandle m_program;
-	bgfx::ShaderHandle dmapack_vs = bgfx::createEmbeddedShader(&dg_dmapack_vs_embed, bgfx::RendererType::Direct3D11, "dg_dmapack_vs");
-	bgfx::ShaderHandle dmapack_fs = bgfx::createEmbeddedShader(&dg_dmapack_fs_embed, bgfx::RendererType::Direct3D11, "dg_dmapack_fs");
-	m_program = bgfx::createProgram(dmapack_vs, dmapack_fs, true);
+	bgfx::ProgramHandle program;
+	bgfx::ShaderHandle shader_vs = bgfx::createEmbeddedShader(&shader_vs_embed, bgfx::RendererType::Direct3D11, "shader_vs");
+	bgfx::ShaderHandle shader_fs = bgfx::createEmbeddedShader(&shader_fs_embed, bgfx::RendererType::Direct3D11, "shader_fs");
+	program = bgfx::createProgram(shader_vs, shader_fs, true);
 
 	// Clear
 	const bgfx::ViewId kClearView = 0;
-	bgfx::setViewClear(kClearView, BGFX_CLEAR_COLOR);
+	bgfx::setViewClear(kClearView, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH);
 	bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
 
 	// Loop
@@ -82,41 +67,40 @@ void render(GLFWwindow* window)
 		glfwPollEvents();
 		bgfx::touch(kClearView);
 
-		bgfx::setVertexBuffer(0, m_vbh);
-		bgfx::setState(0 | BGFX_STATE_WRITE_R | BGFX_STATE_WRITE_G | BGFX_STATE_WRITE_B | UINT64_C(0));
-		bgfx::submit(0, m_program);
+		bgfx::setVertexBuffer(0, vbh);
+		bgfx::submit(0, program);
 
 		// Advance to next frame. Process submitted rendering primitives.
 		bgfx::frame();
 	}
 
 	// Clean
-	bgfx::destroy(m_vbh);
-	bgfx::destroy(m_program);
+	bgfx::destroy(vbh);
+	bgfx::destroy(program);
 }
 
 int main(int argc, char **argv)
 {
-	// Init
 	if (!glfwInit())
 		return 1;
+
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
 	GLFWwindow *window = glfwCreateWindow(1024, 768, "BGFX test", nullptr, nullptr);
 	if (!window)
 		return 1;
+
 	glfwSetKeyCallback(window, glfw_keyCallback);
+
 	bgfx::renderFrame();
+	
 	bgfx::Init init;
 	init.platformData.nwh = glfwGetWin32Window(window);
-	int width, height;
-	glfwGetWindowSize(window, &width, &height);
-	init.resolution.width = (uint32_t)width;
-	init.resolution.height = (uint32_t)height;
-	init.resolution.reset = BGFX_RESET_VSYNC;
+
 	if (!bgfx::init(init))
 		return 1;
 
-	render(window);
+	mainLoop(window);
 
     bgfx::shutdown();
     glfwTerminate();
